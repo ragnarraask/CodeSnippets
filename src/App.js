3826f4f9e64d181135d6666c6dbe71b948375cd4
martinojaste <martin.ojaste@gmail.com>
2019-02-02 15:46:05 +0000

    updatwes, fuzzysearch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css'
 import Snippet from './Snippet'
 import Footer from './Footer'
 import Header from './Header'
+import fuzzySearch from  'fuzzysearch'
+
 
 const styles = theme => ({
   cardGrid: {
@@ -32,31 +34,28 @@ class App extends Component {
     this.state = {
       snippets: [],
       filteredSnippets: [],
-      isFiltering: false,
-      isLoading: true,
-
+      isLoading: true
     }
 
-
-    this.filterSnippets = (event) => {
+    this.filterSnippets = event => {
       const q = event.target.value.toLowerCase()
-  
-      const filteredSnippets = this.state.snippets.filter(snippet =>
-        snippet.name.toLowerCase().startsWith(q)
+
+      const filteredSnippets = this.state.snippets.filter(snippet => fuzzySearch(q, snippet.name.toLowerCase())
+
       )
       console.log(filteredSnippets)
-      this.setState({filteredSnippets })
+      this.setState({ filteredSnippets })
     }
-
-
-    
   }
   componentDidMount() {
     fetch(
       'http://192.168.1.124:8080/api/collections/get/snippets?token=84588594ba67fc351bd6d464bb6b3a'
     )
       .then(response => response.json())
-      .then(data => this.setState({ snippets: data.entries, filteredSnippets: data.entries, isLoading: false }))
+      .then(data => {
+        this.setState({ snippets: data.entries,  filteredSnippets: data.entries.reverse(), isLoading: false })
+
+      })
   }
 
   render(props) {
@@ -64,14 +63,9 @@ class App extends Component {
     return (
       <React.Fragment>
         <CssBaseline />
-        <Header
-          filterSnippets={this.filterSnippets}
-        />
+        <Header filterSnippets={this.filterSnippets} />
         <main className="main">
-          <Snippet
-            filteredSnippets={filteredSnippets}
-            isLoading={isLoading}
-          />
+          <Snippet filteredSnippets={filteredSnippets} isLoading={isLoading} />
         </main>
         <Footer />
       </React.Fragment>
--- src/SnippetTags.js
+++ src/SnippetTags.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Link from '@material-ui/core/Link'
 
 const SnippetTags = props => {
+  const { tags = [] } = props
+  if(tags.length === 0){
+      return ""
+  }
   return (
-      <ul>
-        {props.tags.map((tag, id) => (
-          <li key={id}>
-            <Link to="{IDBCursortag}">{tag}</Link>
-          </li>
-        ))}
-      </ul>
+    <ul>
+      {tags.map((tag, id) => (
+        <li key={id}>
+          <Link to="{tag}">{tag}</Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 export default SnippetTags
-
-
