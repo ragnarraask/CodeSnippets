a842fa419976e35e65d3f9ee745566ef34ae611f
Ragnar <ragnar.raask@gmail.com>
2019-02-02 13:10:10 +0200

    AddSnippet

--- src/AddSnippet.js
+++ src/AddSnippet.js
@@ -4,27 +4,39 @@ import React, { Component } from "react";
 class Snipper extends Component {
     constructor() {
         super();
-
         this.state = {
-            snippet: {}
+            snippetName: "",
+            code: ""
         };
+        this.snippet = {
+            snippetName: "",
+            code: ""
+        }
     }
 
     addSnippetHandler() {
-        //fetch('http://192.168.1.124:8080/api/collections/get/snippets?token=84588594ba67fc351bd6d464bb6b3a')
-           // .then(response => response.json())
-            //.then(data => this.setState({ snippets: data.entries, isLoading: false }));
+        console.log(this.snippet)
+        fetch('http://192.168.1.124:8080/api/collections/get/snippets?token=84588594ba67fc351bd6d464bb6b3a', {
+            method: 'POST',
+            headers: new Headers(),
+            body: JSON.stringify({ name: this.state.snippetName, snippet: this.state.code })
+        }).then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err))
     }
+
 
     render() {
         //const { snippets, isLoading } = this.state;
 
         return (
             <div>
-                <h1>Add your own Super snippet</h1>
-               
-                <input></input>
-                <button onClick ={this.addSnippetHandler}>Add snippet</button>
+                <p>Add your own Super snippet</p>
+                <p>Add your snippet name:</p>
+                <input ></input>
+                <p>Add your snippet code:</p>
+                <input ></input>
+                <button onClick={() => this.addSnippetHandler()}>Add snippet</button>
             </div>
         )
     }
