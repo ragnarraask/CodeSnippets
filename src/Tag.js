a265c77d6dea8c609eeb2db7218b40c0e8f290cd
Ragnar <ragnar.raask@gmail.com>
2019-02-02 14:05:49 +0200

    Router

---
+++ src/Tag.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+
+class Tag extends Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Tag</p>
+                
+            </div>
+        )
+    }
+}
+
+export default Tag
