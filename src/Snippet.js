f2df6d9cc1caca5273623244372bd9800a5d0619
Ragnar <ragnar.raask@gmail.com>
2019-02-02 12:08:36 +0200

    Snippet fetching

--- src/Snippet.js
+++ src/Snippet.js
@@ -6,16 +6,30 @@ class Snipper extends Component {
         super();
 
         this.state = {
-
+            snippets: [],
+            isLoading: true
         };
     }
 
-    render() {
+    componentDidMount() {
+        fetch('http://192.168.1.124:8080/api/collections/get/snippets?token=84588594ba67fc351bd6d464bb6b3a')
+            .then(response => response.json())
+            .then(data => this.setState({ snippets: data.entries, isLoading: false }));
+    }
 
+    render() {
+        const { snippets, isLoading } = this.state;
+
+        if (isLoading) {
+            return <p>Loading ...</p>;
+        }
         return (
             <div>
                 <h1>Super snippet</h1>
-                <input></input>
+               
+                {snippets.map(snippet =>
+                        <p> {snippet.snippet}</p>
+                )}
             </div>
         )
     }
